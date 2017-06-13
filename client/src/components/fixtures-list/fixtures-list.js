e50e1e8da319fdbc11b6f53783ce4c65a1809b0e
copperkraft <copperkraft@gmail.com>
2017-06-13 14:28:57 +0300

    Added fixture list component

---
+++ client/src/components/fixtures-list/fixtures-list.js
@@ -0,0 +1,8 @@
+import ko from 'knockout';
+
+import template from 'components/fixtures-tab/fixtures-tab.html';
+
+function TeamsViewModel(params) {
+    console.log(params);
+}
+export {TeamsViewModel as viewModel, template};
--- server/api/api-configs/team-fixtures.js
+++ server/api/api-configs/team-fixtures.js
@@ -2,7 +2,7 @@ module.exports = {
     params: params => {
         return {
             //http://api.football-data.org/v1/teams/66/fixtures
-            uri: `http://api.football-data.org/v1/teams/${params.id}/fixtures?timeFrame=${params.timeFrame || 14}`,
+            uri: `http://api.football-data.org/v1/teams/${params.id}/fixtures?timeFrame=${params.timeFrame || 'n14'}`,
             method: 'GET',
             headers: {
                 'x-auth-token': 'ea247d3972de4c13b075ae1b61b88d72',
