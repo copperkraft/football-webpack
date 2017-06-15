b9ced7a3330fa231a992d6d89133b770d33972c1
copperkraft <copperkraft@gmail.com>
2017-06-15 15:51:36 +0300

    Added datepicker parser

--- client/src/components/players-list/players-list.js
+++ client/src/components/players-list/players-list.js
@@ -7,5 +7,15 @@ import {teamPlayers} from 'models/team-players';
 
 function TeamsViewModel(params) {
     this.players = teamPlayers.get(params.id);
+    this.pageSize = ko.observable(5);
+    this.currentPage = ko.observable(0);
+    this.pageCount = ko.computed(() => Math.ceil(this.players().length / this.pageSize()));
+    this.nextPage = () => this.currentPage(this.pageCount() - 1 > this.currentPage() ? this.currentPage() + 1 : this.currentPage());
+    this.previousPage = () => this.currentPage(this.currentPage() > 0 ? this.currentPage() - 1 : 0);
+    this.toFirstPage = () => this.currentPage(0);
+    this.toLastPage = () => this.currentPage(this.pageCount() - 1);
+    this.displayedPlayers = ko.computed(() => this.players().filter((item, index) => {
+        return index >= this.currentPage() * this.pageSize() && index < (this.currentPage() + 1) * this.pageSize();
+    }));
 }
 export {TeamsViewModel as viewModel, template};
