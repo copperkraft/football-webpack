c6d6ca490881a1b555437f147e60134e103c26d8
copperkraft <copperkraft@gmail.com>
2017-06-19 10:32:09 +0300

    Added paged list.

--- client/src/components/fixtures-tab/fixtures-tab.js
+++ client/src/components/fixtures-tab/fixtures-tab.js
@@ -4,55 +4,45 @@ import template from 'components/fixtures-tab/fixtures-tab.html';
 import {fixturesList} from 'models/fixtures-list';
 import Pikaday from 'pikaday';
 
-function TeamsViewModel(params) {
-    this.season = {
-        from: new Date('09.01.2016'),
-        to: new Date('05.31.2017')
-    };
-    this.dateFrom = ko.observable(this.season.from.toDateString());
-    this.dateTo = ko.observable(this.season.to.toDateString());
+class TeamsViewModel {
+    constructor(params) {
+        this.season = {
+            from: new Date('09.01.2016'),
+            to: new Date('05.31.2017')
+        };
+        this.dateFrom = ko.observable(this.season.from.toDateString());
+        this.dateTo = ko.observable(this.season.to.toDateString());
 
-    new Pikaday({
-        field: document.getElementsByClassName('js-datepicker-from')[0],
-        minDate: this.season.from,
-        maxDate: this.season.to,
-        defaultDate: this.season.from,
-        toString: date => date.toDateString(),
-        parse: (dateString) => new Date(dateString)
-    });
-    new Pikaday({
-        field: document.getElementsByClassName('js-datepicker-to')[0],
-        minDate: this.season.from,
-        maxDate: this.season.to,
-        defaultDate: this.season.to,
-        toString: date => date.toDateString(),
-        parse: (dateString) => new Date(dateString)
-    });
+        new Pikaday({
+            field: document.getElementsByClassName('js-datepicker-from')[0],
+            minDate: this.season.from,
+            maxDate: this.season.to,
+            defaultDate: this.season.from,
+            toString: date => date.toDateString(),
+            parse: (dateString) => new Date(dateString)
+        });
+        new Pikaday({
+            field: document.getElementsByClassName('js-datepicker-to')[0],
+            minDate: this.season.from,
+            maxDate: this.season.to,
+            defaultDate: this.season.to,
+            toString: date => date.toDateString(),
+            parse: (dateString) => new Date(dateString)
+        });
 
-    this.fixtures = fixturesList.get(params.id);
+        this.fixtures = fixturesList.get(params.id);
 
-    const relevantFixtures = ko.pureComputed(() => {
-        if (this.fixtures && this.fixtures()) {
-            return this.fixtures().filter(fixture => {
-                return fixture.date > new Date(this.dateFrom()) &&
-                    fixture.date < new Date(this.dateTo());
-            });
-        }
-    });
+        this.relevantFixtures = ko.pureComputed(() => {
+            if (this.fixtures && this.fixtures()) {
+                return this.fixtures().filter(fixture => {
+                    return fixture.date > new Date(this.dateFrom()) &&
+                        fixture.date < new Date(this.dateTo());
+                });
+            }
+        });
 
-    this.pageSize = ko.observable(5);
-    this.currentPage = ko.observable(0);
-    this.pageCount = ko.computed(() => Math.ceil(relevantFixtures().length / this.pageSize()));
-    this.nextPage = () => this.currentPage(this.pageCount() - 1 > this.currentPage() ? this.currentPage() + 1 : this.currentPage());
-    this.previousPage = () => this.currentPage(this.currentPage() > 0 ? this.currentPage() - 1 : 0);
-    this.toFirstPage = () => this.currentPage(0);
-    this.toLastPage = () => this.currentPage(this.pageCount() - 1);
-    this.displayedFixtures = ko.computed(() => relevantFixtures().filter((item, index) => {
-        return index >= this.currentPage() * this.pageSize() && index < (this.currentPage() + 1) * this.pageSize();
-    }));
-
-
-    this.selectedFixture = ko.observable();
-    this.id = params.id;
+        this.selectedFixture = ko.observable();
+        this.id = params.id;
+    }
 }
 export {TeamsViewModel as viewModel, template};
--- client/src/components/list-paginator/list-paginator.js
+++ client/src/components/list-paginator/list-paginator.js
@@ -4,15 +4,17 @@ import template from 'components/list-paginator/list-paginator.html';
 
 class TeamsViewModel {
     constructor(params) {
-        console.log(params);
         this.array = params.array;
-        this.pageSize = params.pageSize || 10;
+        this.parentComponent = params.parentComponent;
+        this.pageSize = 5;
         this.currentPage = ko.observable(0);
-        this.pageCount = Math.ceil(params.array.length / this.pageSize);
-
-        this.displayedPlayers = ko.pureComputed(() => this.players().filter((item, index) => {
-            return index >= this.currentPage() * this.pageSize() && index < (this.currentPage() + 1) * this.pageSize();
-        }));
+        this.displayedItems = ko.pureComputed(() => {
+            const first = this.currentPage() * this.pageSize;
+            return this.array().slice(first, first + this.pageSize);
+        });
+        this.pageCount = ko.pureComputed(() => {
+            return Math.ceil(this.array().length / this.pageSize);
+        });
     }
 
     goToPreviousPage() {
@@ -22,7 +24,7 @@ class TeamsViewModel {
     }
 
     goToNextPage() {
-        if (this.pageCount - 1 > this.currentPage()) {
+        if (this.pageCount() - 1 > this.currentPage()) {
             this.currentPage(this.currentPage() + 1);
         }
     }
@@ -32,7 +34,7 @@ class TeamsViewModel {
     }
 
     goToLastPage() {
-        return this.currentPage(this.pageCount - 1);
+        return this.currentPage(this.pageCount() - 1);
     }
 }
 
