23b938b28cbbe34ddc10dcc318722fda904a0c67
copperkraft <copperkraft@gmail.com>
2017-06-19 11:59:22 +0300

    Added player paged list.

--- client/src/components/players-list/players-list.js
+++ client/src/components/players-list/players-list.js
@@ -8,28 +8,6 @@ import {teamPlayers} from 'models/team-players';
 class TeamsViewModel {
     constructor(params) {
         this.players = teamPlayers.get(params.id);
-        this.pageSize = ko.observable(5);
-        this.currentPage = ko.observable(0);
-        this.pageCount = ko.computed(() => Math.ceil(this.players().length / this.pageSize()));
-        this.displayedPlayers = ko.computed(() => this.players().filter((item, index) => {
-            return index >= this.currentPage() * this.pageSize() && index < (this.currentPage() + 1) * this.pageSize();
-        }));
-    }
-
-    toFirstPage() {
-        return this.currentPage(0);
-    }
-
-    previousPage() {
-        return this.currentPage(this.currentPage() > 0 ? this.currentPage() - 1 : 0);
-    }
-
-    nextPage() {
-        return this.currentPage(this.pageCount() - 1 > this.currentPage() ? this.currentPage() + 1 : this.currentPage());
-    }
-
-    toLastPage() {
-        return this.currentPage(this.pageCount() - 1);
     }
 }
 export {TeamsViewModel as viewModel, template};
