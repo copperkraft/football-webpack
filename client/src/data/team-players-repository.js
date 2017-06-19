f2e0cf83d22a7f22291946fbb89c32d52125e5e0
copperkraft <copperkraft@gmail.com>
2017-06-19 13:17:20 +0300

    Returned sort in players

--- client/src/data/team-players-repository.js
+++ client/src/data/team-players-repository.js
@@ -2,6 +2,8 @@ import request from 'data/request';
 
 export const teamPlayersRepository = {
     get: teamId => {
-        return request(`api/teams/${teamId}/players`);
+        return request(`api/teams/${teamId}/players`).then(response => {
+            return response.sort((a, b) => a.jerseyNumber > b.jerseyNumber ? 1 : -1);
+        });
     }
 };
