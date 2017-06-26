e9c532f4cee9a96280254b03e3c3efe7647a891b
copperkraft <copperkraft@gmail.com>
2017-06-26 14:26:22 +0300

    Remove unnecessary js from html template in navigation.

--- client/src/components/header-navigation/header-navigation.js
+++ client/src/components/header-navigation/header-navigation.js
@@ -4,10 +4,23 @@ import register from 'components/component-registrator';
 
 class ViewModel {
     constructor() {
+        this.links = [
+            {
+                text: 'league table',
+                reference: '#league'
+            },{
+                text: 'teams',
+                reference: '#teams'
+            }
+        ];
         this.url = ko.observable(location.hash);
         window.addEventListener('hashchange', () => {
             this.url(location.hash);
         });
+    }
+
+    isCurrent(url) {
+        return url === this.url();
     }
 }
 
