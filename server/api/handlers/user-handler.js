c87c7764e431cbf0079b397ebd2720a5d8ac6f68
copperkraft <copperkraft@gmail.com>
2017-07-05 13:40:03 +0300

    Removed unused console logging

--- server/api/handlers/user-handler.js
+++ server/api/handlers/user-handler.js
@@ -3,7 +3,6 @@ const user = require('../../models/user');
 module.exports.get = (request, response) => {
     user.get(request.session)
         .then(data => {
-            console.log(data);
             response.send(data);
         })
         .catch(() => response.sendStatus(403));
