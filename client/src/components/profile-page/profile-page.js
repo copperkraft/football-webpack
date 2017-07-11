9048d6e8499c729689bb459e908b1dbb201004d3
copperkraft <copperkraft@gmail.com>
2017-07-11 13:23:59 +0300

    Added displaying information to profile page

--- client/src/components/profile-page/profile-page.js
+++ client/src/components/profile-page/profile-page.js
@@ -1,16 +1,19 @@
+import ko from 'knockout';
+
 import './profile-page.less';
 import template from 'components/profile-page/profile-page.html';
 import register from 'components/component-registrator';
 
 import {user} from 'providers/user-provider';
+import {favorites} from 'providers/favorites-provider';
 
 class ProfilePageViewModel {
+
     constructor() {
         this.user = user.currentUser;
         user.get();
-        this.user.subscribe(value => {
-            console.log(value);
-        });
+        this.favorites = ko.observableArray();
+        favorites.get().then(data => this.favorites(data));
     }
 }
 
