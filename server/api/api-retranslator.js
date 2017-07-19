0487c70ea9fdfeb81c73bb0a024def306319225b
copperkraft <copperkraft@gmail.com>
2017-07-19 10:48:57 +0300

    Added pagination logic to server

--- server/api/api-retranslator.js
+++ server/api/api-retranslator.js
@@ -1,9 +1,15 @@
 const rp = require('request-promise-native');
+const paging = require('./paginator');
 
 module.exports = (config) => (request, response) => {
     config.params(request.params).then(params => {
-        rp(params).then(data => {
-            response.send(config.mapper ? config.mapper(data) : data);
-        }).catch(err => console.log(err.message + ' ...while fetching data from api ' + params));
+        rp(params)
+            .then(data => config.mapper ? config.mapper(data) : data)
+            .then(data => config.filter ? config.filter(request.query.filter) : data)
+            .then(data => paging(data, request.query.paging))
+            .then(data => {
+                response.send(data);
+            })
+            .catch(err => console.log(err.message + ' ...while fetching data from api ' + params));
     });
 };
