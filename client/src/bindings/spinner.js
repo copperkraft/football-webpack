642f305d82390ba9dde247d08763f98ae5804536
copperkraft <copperkraft@gmail.com>
2017-07-20 12:17:04 +0300

    Changed approach to spinner binding

--- client/src/bindings/spinner.js
+++ client/src/bindings/spinner.js
@@ -2,18 +2,14 @@ import ko from 'knockout';
 import Spinner from 'spin';
 
 ko.bindingHandlers.spinner = {
-    init(element, valueAccessor) {
-        const spinner = new Spinner();
-
-        spinner.spin(element);
-
-        valueAccessor().subscribe(value => {
-            if(!value) {
-                spinner.spin(element);
-            } else {
-                spinner.stop();
-            }
-        });
-
+    init(element) {
+        element.spinner = new Spinner();
+    },
+    update(element, valueAccessor) {
+        if(!valueAccessor()()) {
+            element.spinner.spin(element);
+        } else {
+            element.spinner.stop();
+        }
     }
 };
