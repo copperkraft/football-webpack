955ffc6c2d2a7d4aaafb7a4c9e15fb2516bbeca3
copperkraft <copperkraft@gmail.com>
2017-07-24 10:01:00 +0300

    Remove error logging in request util

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -4,17 +4,10 @@ export default {
             credentials: 'include'
         }).then(response => {
             switch (response.status) {
-                case 401:
-                    throw 'not authorized';
-                case 403:
-                    throw 'forbidden';
                 case 200:
                     return response.json();
                 default:
             }
-        }).catch(error => {
-            console.log(error);
-            throw error;
         });
     },
     post(url, data) {
@@ -28,17 +21,10 @@ export default {
             body: JSON.stringify(data)
         }).then(response => {
             switch (response.status) {
-                case 401:
-                    throw 'not authorized';
-                case 403:
-                    throw 'forbidden';
                 case 200:
                     return response.json();
                 default:
             }
-        }).catch(error => {
-            console.log(error);
-            throw error;
         });
     }
 };
