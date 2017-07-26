4bf3413b8695ffba8de93aae13632aceac9fb60d
copperkraft <copperkraft@gmail.com>
2017-07-26 14:38:01 +0300

    Removed duplicate using of valueAccessor

--- client/src/bindings/calendar.js
+++ client/src/bindings/calendar.js
@@ -4,14 +4,14 @@ import Pikaday from 'pikaday';
 ko.bindingHandlers.calendar = {
     init(element, valueAccessor) {
         let {from, to, value} = valueAccessor();
-        [from, to, value] = [from, to, value].map(item => ko.unwrap(item));
+        [from, to] = [from, to].map(item => ko.unwrap(item));
 
-        element.value = value.toDateString();
+        element.value = value().toDateString();
 
         element.picker = new Pikaday({
             yearRange: [1900, 2020],
             field: element,
-            defaultDate: value,
+            defaultDate: value(),
             minDate: from,
             maxDate: to,
             toString: date => date ? date.toDateString() : '',
@@ -23,7 +23,7 @@ ko.bindingHandlers.calendar = {
         });
 
         element.addEventListener('change', () => {
-            valueAccessor().value(element.value);
+            value(element.value);
         });
     }
 };
