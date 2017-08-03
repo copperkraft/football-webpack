db1d97c55f3957fcf574bebd967893650bf9e215
copperkraft <copperkraft@gmail.com>
2017-08-03 12:22:57 +0300

    Added t-shirt image

--- client/src/components/player-card/player-card.js
+++ client/src/components/player-card/player-card.js
@@ -11,8 +11,11 @@ export default function PlayerCard(props) {
     const player = props.player;
     return (
         <div className="team-card">
+            <div className="team-card__t-shirt">
+                {player.jerseyNumber ? player.jerseyNumber : '?'}
+            </div>
             <div className="team-card__full-name">
-                {player.jerseyNumber}: {player.name}
+                {player.name}
             </div>
             <div className="team-card__info">
                 position: {player.position}
