d00d0a56bcae737ee267bbe4a8030c89a4afb5cd
copperkraft <copperkraft@gmail.com>
2017-08-07 13:32:00 +0300

    Added error handling to user redux

--- client/src/actions/user-actions.js
+++ client/src/actions/user-actions.js
@@ -22,47 +22,71 @@ function receiveUser(user) {
     };
 }
 
+export const FAILED_RECEIVE_USER = 'FAILED_RECEIVE_USER';
+function failedReceiveUser() {
+    return {
+        type: FAILED_RECEIVE_USER
+    };
+}
+
 export function fetchUser() {
     return function(dispatch) {
         dispatch(requestUser());
-        return userProvider.get().then(user => {
-            dispatch(receiveUser(user));
-        });
+        return userProvider.get()
+            .then(user => {
+                dispatch(receiveUser(user));
+            })
+            .catch(error => {
+                console.error(error);
+                dispatch(failedReceiveUser());
+            });
     };
 }
 
 export function registerUser(info) {
     return function(dispatch) {
         dispatch(requestUser());
-        return userProvider.register(info).then(user => {
-            dispatch(receiveUser(user));
-        });
+        return userProvider.register(info)
+            .then(user => {
+                dispatch(receiveUser(user));
+            })
+            .catch(error => {
+                console.error(error);
+                dispatch(failedReceiveUser());
+            });
     };
 }
 
 export function logInUser(info) {
     return function(dispatch) {
         dispatch(requestUser());
-        return userProvider.register(info).then(user => {
-            dispatch(receiveUser(user));
-        });
+        return userProvider.register(info)
+            .then(user => {
+                dispatch(receiveUser(user));
+            })
+            .catch(error => {
+                console.error(error);
+                dispatch(failedReceiveUser());
+            });
     };
 }
 
 export function logOutUser() {
     return function(dispatch) {
         dispatch(userLogout());
-        return userProvider.register(info).then(user => {
-            dispatch(receiveUser(user));
-        });
     };
 }
 
 export function setUserInfo(info) {
     return function(dispatch) {
         dispatch(requestUser());
-        return userProvider.set(info).then(user => {
-            dispatch(receiveUser(user));
-        });
+        return userProvider.set(info)
+            .then(user => {
+                dispatch(receiveUser(user));
+            })
+            .catch(error => {
+                console.error(error);
+                dispatch(failedReceiveUser());
+            });
     };
 }
--- client/src/reducers/user.js
+++ client/src/reducers/user.js
@@ -1,11 +1,13 @@
 import {
     RECEIVE_USER,
     REQUEST_USER,
-    USER_LOGOUT
+    USER_LOGOUT,
+    FAILED_RECEIVE_USER
 } from '../actions/user-actions';
 
 const initialState = {
     isFetching: false,
+    isFailed: false,
     user: null
 };
 
@@ -14,17 +16,26 @@ export function user(state = initialState, action) {
         case REQUEST_USER:
             return {
                 isFetching: true,
-                user: null
+                user: null,
+                isFailed: false
             };
         case RECEIVE_USER:
             return {
                 isFetching: false,
-                user: action.payload
+                user: action.payload,
+                isFailed: false
             };
         case USER_LOGOUT:
             return {
                 isFetching: false,
-                user: null
+                user: null,
+                isFailed: false
+            };
+        case FAILED_RECEIVE_USER:
+            return {
+                isFetching: false,
+                user: null,
+                isFailed: true
             };
         default:
             return state;
