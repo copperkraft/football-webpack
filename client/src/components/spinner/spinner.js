e273923b099e6f69c25b61a881a44a66ccc3af8c
copperkraft <copperkraft@gmail.com>
2017-08-11 16:33:14 +0300

    Used ref callback in spinner component

--- client/src/components/spinner/spinner.js
+++ client/src/components/spinner/spinner.js
@@ -3,14 +3,9 @@ import Spinner from 'spin';
 import './spinner.less';
 
 export default class Spin extends Component {
-    componentDidMount() {
-        this.spinner = new Spinner();
-        this.spinner.spin(this.refs.container);
-    }
-    componentWillUnmount() {
-        this.spinner.stop();
-    }
     render() {
-        return <div className="spinner" ref="container"/>;
+        return <div className="spinner" ref={container => {
+            new Spinner().spin(container);
+        }}/>;
     }
 }
