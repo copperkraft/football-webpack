9f65e19af53c6083f6526b950373a36eca138852
copperkraft <copperkraft@gmail.com>
2017-08-14 17:16:12 +0300

    Fixed problem with wrong goals number

--- client/src/models/fixture/fixture-mapper.js
+++ client/src/models/fixture/fixture-mapper.js
@@ -8,7 +8,7 @@ export default dataObject => {
         awayTeamName: dataObject.awayTeamName,
         //Used ternary instead of a Boolean because the number of goals can be 0
         goalsHomeTeam: dataObject.result ? dataObject.result.goalsHomeTeam : dataObject.goalsHomeTeam,
-        goalsAwayTeam: dataObject.result ? dataObject.result.goalsHomeTeam : dataObject.goalsAwayTeam,
+        goalsAwayTeam: dataObject.result ? dataObject.result.goalsAwayTeam : dataObject.goalsAwayTeam,
         isFinished: dataObject.status === 'FINISHED'
     }) : null;
 };
