0205154bf89ccaec4b068b743efbd8e1fb5f68e1
copperkraft <copperkraft@gmail.com>
2017-08-14 11:10:27 +0300

    Added action creators for favorites

---
+++ client/src/actions/favorite-actions.js
@@ -0,0 +1,25 @@
+import {favorites} from 'providers/favorites-provider';
+
+export const REQUST_FAVORITES = 'REQUST_FAVORITES';
+function requestFavorites() {
+    return {
+        type: REQUST_FAVORITES
+    };
+}
+
+export const RECEIVE_FAVORITES = 'RECEIVE_FAVORITES';
+function receiveFavorites(favorites) {
+    return {
+        type: RECEIVE_FAVORITES,
+        payload: favorites
+    };
+}
+
+export function fetchFavorites() {
+    return function(dispatch) {
+        dispatch(requestFavorites());
+        return favorites.get().then(favorites => {
+            dispatch(receiveFavorites(favorites));
+        });
+    };
+}
